208e007bdf6a3772449b39be0fe56ef589222d8e
SametKula <sametkula@proton.me>
2023-08-09 23:03:51 +0300

    New Classes added(hero classes): All basic base hero classes added ,and class' attributes defined. - Bandit ,Druid ,Elf ,Juggernaut ,and Samurai created. Warrior class modified. - Basic class constructor ,attributes ,getters ,and setters added into the classes - All classes' the attributes are name ,hp ,strength ,defense ,exp ,and level. - Base constructor defined for creating instances of class.

--- src/main/dev/efficienthouse/app/entity/hero/Samurai.java
+++ src/main/dev/efficienthouse/app/entity/hero/Samurai.java
@@ -1,6 +1,6 @@
 package main.dev.efficienthouse.app.entity.hero;
 
-public class Warrior {
+public class Samurai {
     public String name;
     public double hp;
     public int damage;
@@ -8,7 +8,7 @@ public class Warrior {
     public int exp;
     public int level;
 
-    public Warrior(String newName, double newHp, int newStrength, int newDefense, int newExp, int newLevel) {
+    public Samurai(String newName, double newHp, int newStrength, int newDefense, int newExp, int newLevel) {
         name = newName;
         hp = newHp;
         damage = newStrength;
@@ -16,6 +16,7 @@ public class Warrior {
         exp = newExp;
         level = newLevel;
     }
+
 
     public String getName() {
         return name;
@@ -64,5 +65,4 @@ public class Warrior {
     public void setLevel(int newLevel) {
         level = newLevel;
     }
-
 }
